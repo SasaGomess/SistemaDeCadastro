1a4a3ea943c5d42ee1ed70c148e89b04d4012c82
SasaGomes <sabrina.gomess.dev@gmail.com>
2025-05-05 10:38:59 -0300

    feat: criando a classe pet, atributos, construtores, getters e setters

---
+++ src/sabrina/desafio/cadastro/entities/Pet.java
@@ -0,0 +1,89 @@
+package sabrina.desafio.cadastro.entities;
+
+import sabrina.desafio.cadastro.enums.SexoPet;
+import sabrina.desafio.cadastro.enums.TipoPet;
+
+public class Pet {
+    public static final String NAO_INFORMADO = null;
+    private String nomeSobrenome;
+    private TipoPet tipoPet;
+    private SexoPet sexoPet;
+    private Endereco endereco;
+    private Double idade;
+    private Double peso;
+    private String raca;
+
+    public Pet() {
+    }
+
+    public Pet(String nomeSobrenome, TipoPet tipoPet, SexoPet sexoPet, Endereco endereco, Double idade, Double peso, String raca) {
+        this.nomeSobrenome = nomeSobrenome;
+        this.tipoPet = tipoPet;
+        this.sexoPet = sexoPet;
+        this.endereco = endereco;
+        this.idade = idade;
+        this.peso = peso;
+        this.raca = raca;
+    }
+
+    public String getNomeSobrenome() {
+        return nomeSobrenome;
+    }
+
+    public void setNomeSobrenome(String nomeSobrenome) {
+        this.nomeSobrenome = nomeSobrenome;
+    }
+
+    public TipoPet getTipoPet() {
+        return tipoPet;
+    }
+
+    public void setTipoPet(TipoPet tipoPet) {
+        this.tipoPet = tipoPet;
+    }
+
+    public SexoPet getSexoPet() {
+        return sexoPet;
+    }
+
+    public void setSexoPet(SexoPet sexoPet) {
+        this.sexoPet = sexoPet;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public Double getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Double idade) {
+        this.idade = idade;
+    }
+
+    public Double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(Double peso) {
+        this.peso = peso;
+    }
+
+    public String getRaca() {
+        return raca;
+    }
+
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
+
+    @Override
+    public String toString() {
+        return "";
+    }
+}

1549c6b78a9f252cf3ae2f672f64f6129bcb2c4a
SasaGomes <sabrina.gomess.dev@gmail.com>
2025-05-05 12:42:49 -0300

    feat: criando excecoes personalizadas para peso e idade invalidos

---
+++ src/sabrina/desafio/cadastro/exceptions/IdadeException.java
@@ -0,0 +1,7 @@
+package sabrina.desafio.cadastro.exceptions;
+
+public class IdadeException extends RuntimeException{
+    public IdadeException (String msg){
+        super(msg);
+    }
+}
---
+++ src/sabrina/desafio/cadastro/exceptions/PesoException.java
@@ -0,0 +1,8 @@
+package sabrina.desafio.cadastro.exceptions;
+
+public class PesoException extends RuntimeException {
+
+    public PesoException (String msg){
+        super(msg);
+    }
+}

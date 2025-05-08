29dffb5d9d56c93e6cd3e78c4acfe54fc67dc4bd
SasaGomes <sabrina.gomess.dev@gmail.com>
2025-05-08 09:17:14 -0300

    fix: concertando a logica de looping infinito no exibindo menu

---
+++ src/sabrina/desafio/cadastro/services/MenuOpcoes.java
@@ -0,0 +1,38 @@
+package sabrina.desafio.cadastro.services;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class MenuOpcoes {
+    private Scanner scanner = new Scanner(System.in);
+    public int exibindoMenu() {
+        try {
+        int resposta;
+        System.out.println("\nMENU PRINCIPAL");
+        System.out.println("====================");
+        System.out.println("1. Cadastrar um novo Pet");
+        System.out.println("2. Alterar os dados do pet cadastrado");
+        System.out.println("3. Deletar um pet cadastrado");
+        System.out.println("4. Listar todos os pets cadastrados");
+        System.out.println("5. Listar pets por algum critério (idade, nome, raça)");
+        System.out.println("6. Sair");
+        System.out.print("RESPOSTA:");
+        resposta = scanner.nextInt();
+
+            if (scanner.hasNextInt()){
+                if (resposta >= 1 && resposta <= 6){
+                    return resposta;
+                }else {
+                    return exibindoMenu();
+                }
+            }
+            else {
+                throw new InputMismatchException("Valor inválido, o valor deve ser um numero");
+            }
+        }catch (InputMismatchException e){
+            System.out.println("Valor invalido, tente novamente");
+            scanner.next();
+            return exibindoMenu();
+        }
+    }
+}

0757e93a68fef01c269aa9ad8ccd381f78fb0ffc
SasaGomes <sabrina.gomess.dev@gmail.com>
2025-05-12 21:57:13 -0300

    feat: criando interface para busca dos pets

---
+++ src/sabrina/desafio/cadastro/services/BuscarPet.java
@@ -0,0 +1,51 @@
+package sabrina.desafio.cadastro.services;
+
+import sabrina.desafio.cadastro.entities.Pet;
+import sabrina.desafio.cadastro.enums.TipoPet;
+import sabrina.desafio.cadastro.services.interfaces.BuscarPetInterface;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class BuscarPet implements BuscarPetInterface {
+    Scanner scanner = new Scanner(System.in);
+    BuscarPorDoisCriterios buscandoDoisCriterios = new BuscarPorDoisCriterios();
+
+    @Override
+    public void buscandoPet(List<Pet> list, Pet pet) {
+        List<Pet> resultBusca = new ArrayList<>();
+
+        resultBusca =  buscandoCriterioObrigatorio(resultBusca, list);
+
+        System.out.println("Agora digite quantos criterios de busca você deseja [1 ou 2]: ");
+        int criterios = scanner.nextInt();
+        do{
+            if (criterios == 1){
+                BuscarPorUmCriterio.buscandoPorUmCriterio(scanner, resultBusca);
+            }else {
+                buscandoDoisCriterios.buscandoDoisCriterios(scanner, resultBusca);
+            }
+        }while (criterios != 1 || criterios != 2);
+    }
+    public List<Pet> buscandoCriterioObrigatorio(List<Pet> resultBusca, List<Pet> list){
+        System.out.println("Qual o tipo do Pet?(cachorro ou gato): ");
+        String resp = scanner.nextLine();
+        String respBusca = resp.trim().toUpperCase();
+
+        if (respBusca.equalsIgnoreCase("CACHORRO")) {
+            resultBusca = list.stream().filter(x -> x.getTipoPet().equals(TipoPet.CACHORRO)).collect(Collectors.toList());
+        } else if (respBusca.equalsIgnoreCase("GATO")) {
+            resultBusca = list.stream().filter(x -> x.getTipoPet().equals(TipoPet.GATO)).collect(Collectors.toList());
+        } else {
+            System.out.println("O tipo do pet não existe. Por favor escolha entre Gato ou Cachorro");
+        }
+        if (!resultBusca.isEmpty()) {
+            for (int i = 0; i < resultBusca.size(); i++) {
+                System.out.println((i + 1) + ". " + resultBusca.get(i));
+            }
+        }
+        return resultBusca;
+    }
+}

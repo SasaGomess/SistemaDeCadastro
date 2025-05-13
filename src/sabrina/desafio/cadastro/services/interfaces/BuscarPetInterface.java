6568e2cc1ec2181c57abf5f59d1b65341af2b0c6
SasaGomes <sabrina.gomess.dev@gmail.com>
2025-05-12 23:49:21 -0300

    fix: modificando metodo da minha interface para retornar uma lista de pet

--- src/sabrina/desafio/cadastro/services/interfaces/BuscarPetInterface.java
+++ src/sabrina/desafio/cadastro/services/interfaces/BuscarPetInterface.java
@@ -5,5 +5,5 @@ import sabrina.desafio.cadastro.entities.Pet;
 import java.util.List;
 
 public interface BuscarPetInterface {
-    void buscandoPet(List<Pet>list, Pet pet);
+    List<Pet> buscandoPet(List<Pet>list, Pet pet);
 }

5a5c48b8766dea3db26b4c407f85f5e38b01378d
SasaGomes <sabrina.gomess.dev@gmail.com>
2025-05-12 23:43:06 -0300

    update: classe buscar pet agora retorna uma lista de pets ja filtrados

--- src/sabrina/desafio/cadastro/services/BuscarPet.java
+++ src/sabrina/desafio/cadastro/services/BuscarPet.java
@@ -14,20 +14,23 @@ public class BuscarPet implements BuscarPetInterface {
     BuscarPorDoisCriterios buscandoDoisCriterios = new BuscarPorDoisCriterios();
 
     @Override
-    public void buscandoPet(List<Pet> list, Pet pet) {
+    public List<Pet> buscandoPet(List<Pet> list, Pet pet) {
         List<Pet> resultBusca = new ArrayList<>();
-
+        List<Pet> resultBuscaComCriterios = new ArrayList<>();
         resultBusca =  buscandoCriterioObrigatorio(resultBusca, list);
 
         System.out.println("Agora digite quantos criterios de busca você deseja [1 ou 2]: ");
         int criterios = scanner.nextInt();
+
         do{
             if (criterios == 1){
-                BuscarPorUmCriterio.buscandoPorUmCriterio(scanner, resultBusca);
-            }else {
-                buscandoDoisCriterios.buscandoDoisCriterios(scanner, resultBusca);
+                resultBuscaComCriterios = BuscarPorUmCriterio.buscandoPorUmCriterio(scanner, resultBusca);
+            }else if (criterios == 2){
+                resultBuscaComCriterios = buscandoDoisCriterios.buscandoDoisCriterios(scanner, resultBusca);
             }
         }while (criterios != 1 || criterios != 2);
+
+        return resultBuscaComCriterios;
     }
     public List<Pet> buscandoCriterioObrigatorio(List<Pet> resultBusca, List<Pet> list){
         System.out.println("Qual o tipo do Pet?(cachorro ou gato): ");
@@ -42,6 +45,7 @@ public class BuscarPet implements BuscarPetInterface {
             System.out.println("O tipo do pet não existe. Por favor escolha entre Gato ou Cachorro");
         }
         if (!resultBusca.isEmpty()) {
+            System.out.println("Pet(s) encontrado(s)");
             for (int i = 0; i < resultBusca.size(); i++) {
                 System.out.println((i + 1) + ". " + resultBusca.get(i));
             }

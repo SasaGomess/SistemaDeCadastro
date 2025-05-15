c05960b2c1e25b5f96719ec045f6c4deaaa957bf
SasaGomes <sabrina.gomess.dev@gmail.com>
2025-05-15 08:55:23 -0300

    fix: fazendo a busca pelo pet ter relação com o pet cadastrado nos arquivos, arrumando criterios e validação de resposta

--- src/sabrina/desafio/cadastro/services/BuscarPorDoisCriterios.java
+++ src/sabrina/desafio/cadastro/services/BuscarPorDoisCriterios.java
@@ -6,12 +6,16 @@ import java.util.*;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
+import static sabrina.desafio.cadastro.utils.ValidandoEntrada.REGEX_STRINGS;
+
 public class BuscarPorDoisCriterios {
     private String nomeBuscado;
     private String racaBuscada;
     private Double idadeBuscada;
     private Double pesoBuscado;
     private String cidadeBuscada;
+    private String rua;
+    private String numeroCasa;
     private String sexoBuscado;
 
     public List<Pet> buscandoDoisCriterios(Scanner sc, List<Pet> resultPet) {
@@ -21,6 +25,7 @@ public class BuscarPorDoisCriterios {
         int resposta1;
         int resposta2;
 
+        while (true) {
         System.out.println("Quais criterios de busca você deseja");
         System.out.println("1. Nome e/ou Sobrenome");
         System.out.println("2. Sexo");
@@ -29,10 +34,10 @@ public class BuscarPorDoisCriterios {
         System.out.println("5. Raça");
         System.out.println("6. Endereço");
 
-        while (true){
+
             System.out.print("1° critério: ");
             resposta1 = sc.nextInt();
-            System.out.println("2° criterio:");
+            System.out.print("2° criterio: ");
             resposta2 = sc.nextInt();
 
             sc.nextLine();
@@ -44,16 +49,7 @@ public class BuscarPorDoisCriterios {
                 if (predicate1 != null && predicate2 != null) {
                     petsPorDoisCriterios = resultPet.stream().filter(predicate1.and(predicate2)).collect(Collectors.toList());
                 }
-
-                if (!petsPorDoisCriterios.isEmpty()){
-                    System.out.println("Pet(s) encontrado(s): ");
-                    for (int i = 0; i < petsPorDoisCriterios.size(); i++) {
-                        System.out.println((i + 1) + ". " + petsPorDoisCriterios.get(i));
-                    }
-                }
                 break;
-            }else {
-                System.out.println("Digite um valor válido entre [1 e 6]");
             }
         }
         return petsPorDoisCriterios;
@@ -61,34 +57,58 @@ public class BuscarPorDoisCriterios {
 
     public Predicate<Pet> buscarPorDoisPredicados(Scanner sc, int resposta) {
 
-        switch (resposta) {
-            case 1:
-                System.out.println("Digite o nome buscado");
-                nomeBuscado = sc.nextLine();
-                return p -> p.getNomeSobrenome().toUpperCase().trim().contains(nomeBuscado.toUpperCase().trim());
-            case 2:
-                System.out.println("Digite o sexo buscado");
-                sexoBuscado = sc.nextLine();
-                return p -> p.getSexoPet().toString().equalsIgnoreCase(sexoBuscado.toUpperCase().trim());
-            case 3:
-                System.out.println("Digite a idade buscada");
-                idadeBuscada = sc.nextDouble();
-                return p -> p.getIdade().equals(idadeBuscada);
-            case 4:
-                System.out.println("Digite o peso buscado");
-                pesoBuscado = sc.nextDouble();
-                return p -> p.getPeso().equals(pesoBuscado);
-            case 5:
-                System.out.println("Digite a raça buscada");
-                racaBuscada = sc.nextLine();
-                return p -> p.getRaca().toUpperCase().trim().contains(racaBuscada.toUpperCase().trim());
-            case 6:
-                System.out.println("Digite a cidade buscada");
-                cidadeBuscada = sc.nextLine();
-                return p -> p.getEndereco().getCidade().toUpperCase().trim().contains(cidadeBuscada.toUpperCase().trim());
-            default:
-                System.out.println("Valor de criterio inválido digite numeros entre [1] e [6] ");
-                return null;
+        try {
+            switch (resposta) {
+                case 1:
+                    System.out.println("Digite o nome buscado");
+                    nomeBuscado = sc.nextLine();
+                    return p -> p.getNomeSobrenome().toUpperCase().trim().contains(nomeBuscado.toUpperCase().trim().replaceAll(REGEX_STRINGS, ""));
+                case 2:
+                    System.out.println("Digite o sexo buscado");
+                    sexoBuscado = sc.nextLine();
+                    return p -> p.getSexoPet().toString().equalsIgnoreCase(sexoBuscado.toUpperCase().trim());
+                case 3:
+                    System.out.println("Digite a idade buscada");
+                    idadeBuscada = sc.nextDouble();
+                    return p -> p.getIdade().equals(idadeBuscada);
+                case 4:
+                    System.out.println("Digite o peso buscado");
+                    pesoBuscado = sc.nextDouble();
+                    return p -> p.getPeso().equals(pesoBuscado);
+                case 5:
+                    System.out.println("Digite a raça buscada");
+                    racaBuscada = sc.nextLine();
+                    return p -> p.getRaca().toUpperCase().trim().contains(racaBuscada.toUpperCase().trim());
+                case 6:
+                    System.out.println("Por qual endereço deseja fazer a busca?");
+                    System.out.println("[1] Cidade: ");
+                    System.out.println("[2] Numero da casa: ");
+                    System.out.println("[3] Rua: ");
+                    int resp = sc.nextInt();
+                    sc.nextLine();
+                    do {
+                        if (resp == 1) {
+                            System.out.println("Digite a cidade que deseja buscar");
+                            cidadeBuscada = sc.nextLine();
+                            return p -> p.getEndereco().getCidade().toUpperCase().trim().contains(cidadeBuscada.toUpperCase().trim());
+                        } else if (resp == 2) {
+                            System.out.println("Digite o n° da casa");
+                            numeroCasa = sc.nextLine();
+                            return p -> p.getEndereco().getNumeroCasa().contains(numeroCasa.trim());
+                        } else if (resp == 3) {
+                            System.out.println("Digite a rua da casa");
+                            rua = sc.nextLine();
+                            return p -> p.getEndereco().getNumeroCasa().contains(numeroCasa.trim());
+                        }
+                        System.out.println("Endereco inválido digite novamente");
+                        resp = sc.nextInt();
+                    } while (resp != 1 && resp != 2 && resp != 3);
+            }
+            return null;
+        } catch (InputMismatchException | NullPointerException e){
+            System.out.println("O valor digitado é inválido porfavor tente novamente");
+            sc.next();
+            return buscarPorDoisPredicados(sc, resposta);
         }
     }
 }
--- src/sabrina/desafio/cadastro/services/BuscarPorUmCriterio.java
+++ src/sabrina/desafio/cadastro/services/BuscarPorUmCriterio.java
@@ -4,59 +4,79 @@ import sabrina.desafio.cadastro.entities.Endereco;
 import sabrina.desafio.cadastro.entities.Pet;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class BuscarPorUmCriterio {
     public static List<Pet> buscandoPorUmCriterio(Scanner sc, List<Pet> resultBusca){
-        List<Pet> petsPorUmCriterio = new ArrayList<>();
-        System.out.println("Qual criterio de busca você deseja");
-        System.out.println("1. Nome e/ou Sobrenome");
-        System.out.println("2. Sexo");
-        System.out.println("3. Idade");
-        System.out.println("4. Peso");
-        System.out.println("5. Raça");
-        System.out.println("6. Endereço");
-        System.out.print("RESPOSTA: ");
-        int resposta = sc.nextInt();
+        try {
+            List<Pet> petsPorUmCriterio = new ArrayList<>();
+            System.out.println("Qual criterio de busca você deseja");
+            System.out.println("1. Nome e/ou Sobrenome");
+            System.out.println("2. Sexo");
+            System.out.println("3. Idade");
+            System.out.println("4. Peso");
+            System.out.println("5. Raça");
+            System.out.println("6. Endereço");
+            System.out.print("RESPOSTA: ");
+            int resposta = sc.nextInt();
+            sc.nextLine();
 
-        while (resposta <= 6 && resposta >= 1){
-            switch (resposta){
-                case 1:
-                    String nomeBuscado = sc.nextLine();
-                    petsPorUmCriterio = resultBusca.stream().filter(p -> p.getNomeSobrenome().toUpperCase().trim().contains(nomeBuscado.toUpperCase().trim())).collect(Collectors.toList());
-                    break;
-                case 2:
-                    String sexoBuscado = sc.nextLine();
-                    petsPorUmCriterio = resultBusca.stream().filter(p -> p.getSexoPet().toString().equalsIgnoreCase(sexoBuscado.toUpperCase().trim())).collect(Collectors.toList());
-                    break;
-                case 3:
-                    Double idadeBuscada = sc.nextDouble();
-                    petsPorUmCriterio = resultBusca.stream().filter(p -> p.getIdade().equals(idadeBuscada)).collect(Collectors.toList());
-                    break;
-                case 4:
-                    Double pesoBuscado = sc.nextDouble();
-                    petsPorUmCriterio = resultBusca.stream().filter(p -> p.getPeso().equals(pesoBuscado)).collect(Collectors.toList());
-                    break;
-                case 5:
-                    String racaBucada = sc.nextLine();
-                    petsPorUmCriterio = resultBusca.stream().filter(p -> p.getRaca().toUpperCase().trim().contains(racaBucada.toUpperCase().trim())).collect(Collectors.toList());
-                    break;
-                case 6:
-                    String enderecoBuscado = sc.nextLine();
-                    petsPorUmCriterio = resultBusca.stream().filter(p -> p.getEndereco().getCidade().toUpperCase().trim().contains(enderecoBuscado.toUpperCase().trim())).collect(Collectors.toList());
-                    break;
-                default:
-                    System.out.println("Pet não encontrado, tente novamente: ");
-                    buscandoPorUmCriterio(sc, resultBusca);
-                    break;
+            if (resposta <= 6 && resposta >= 1) {
+                switch (resposta) {
+                    case 1:
+                        System.out.println("Digite o nome ou sobrenome procurado");
+                        String nomeBuscado = sc.nextLine();
+                        return petsPorUmCriterio = resultBusca.stream().filter(p -> p.getNomeSobrenome().toUpperCase().trim().contains(nomeBuscado.toUpperCase().trim())).collect(Collectors.toList());
+                    case 2:
+                        System.out.println("Digite o sexo procurado");
+                        String sexoBuscado = sc.nextLine();
+                        return petsPorUmCriterio = resultBusca.stream().filter(p -> p.getSexoPet().toString().equalsIgnoreCase(sexoBuscado.toUpperCase().trim())).collect(Collectors.toList());
+                    case 3:
+                        System.out.println("Digite a idade procurada");
+                        String idadeBuscada = sc.nextLine();
+                        return petsPorUmCriterio = resultBusca.stream().filter(p -> p.getIdade().toString().replaceAll(",", "").contains(idadeBuscada.replaceAll(",",""))).collect(Collectors.toList());
+                    case 4:
+                        System.out.println("Digite o peso procurado");
+                        Double pesoBuscado = sc.nextDouble();
+                        return petsPorUmCriterio = resultBusca.stream().filter(p -> p.getPeso().toString().replaceAll(",", "").contains(pesoBuscado.toString().replaceAll(",",""))).collect(Collectors.toList());
+                    case 5:
+                        System.out.println("Digite a raça procurada");
+                        String racaBucada = sc.nextLine();
+                        return petsPorUmCriterio = resultBusca.stream().filter(p -> p.getRaca().toUpperCase().trim().contains(racaBucada.toUpperCase().trim())).collect(Collectors.toList());
+                    case 6:
+                        System.out.println("Digite qual parte do endereco deseja buscar");
+                        System.out.println("[1] Cidade: ");
+                        System.out.println("[2] Numero da casa: ");
+                        System.out.println("[3] Rua: ");
+                        int resp = sc.nextInt();
+                        sc.nextLine();
+                        do {
+                            if (resp == 1) {
+                                System.out.println("Digite a cidade que deseja buscar");
+                                String cidadeBuscada = sc.nextLine();
+                                return petsPorUmCriterio = resultBusca.stream().filter(p -> p.getEndereco().getCidade().toUpperCase().trim().contains(cidadeBuscada.toUpperCase().trim())).toList();
+                            } else if (resp == 2) {
+                                System.out.println("Digite o n° da casa");
+                                String numeroCasa = sc.nextLine();
+                                return  petsPorUmCriterio = resultBusca.stream().filter(p -> p.getEndereco().getNumeroCasa().toUpperCase().trim().contains(numeroCasa.toUpperCase().trim())).toList();
+                            } else if (resp == 3) {
+                                System.out.println("Digite a rua da casa");
+                                String rua = sc.nextLine();
+                                return  petsPorUmCriterio = resultBusca.stream().filter(p -> p.getEndereco().getRua().toUpperCase().trim().contains(rua.toUpperCase().trim())).toList();
+                            }
+                            System.out.println("Endereco inválido digite novamente");
+                            resp = sc.nextInt();
+                        } while (resp != 1 && resp != 2 && resp != 3);
+                }
             }
-            System.out.println("Pet(s) encontrado(s): ");
-            for (int i = 0; i < petsPorUmCriterio.size(); i++) {
-                System.out.println((i+1)+ ". " + petsPorUmCriterio.get(i));
-            }
+            return petsPorUmCriterio;
+        }catch (InputMismatchException e){
+            System.out.println("Pet não encontrado, ou entrada inválida tente novamente Por Favor: ");
+            sc.next();
+            return buscandoPorUmCriterio(sc, resultBusca);
         }
-        return petsPorUmCriterio;
     }
 }

a91baa8207f202db67c5f0b03b205752340452b8
SasaGomes <sabrina.gomess.dev@gmail.com>
2025-05-15 09:45:53 -0300

    refactor: mudando comportamento do meu metodo

--- src/sabrina/desafio/cadastro/services/interfaces/BuscarPetInterface.java
+++ src/sabrina/desafio/cadastro/services/interfaces/BuscarPetInterface.java
@@ -3,7 +3,8 @@ package sabrina.desafio.cadastro.services.interfaces;
 import sabrina.desafio.cadastro.entities.Pet;
 
 import java.util.List;
+import java.util.Scanner;
 
 public interface BuscarPetInterface {
-    List<Pet> buscandoPet(List<Pet>list, Pet pet);
+    List<Pet> buscandoPet( Scanner scanner);
 }

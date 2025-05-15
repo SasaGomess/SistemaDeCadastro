613b9314d65f2ad4056286c21658b859c67c10fe
SasaGomes <sabrina.gomess.dev@gmail.com>
2025-05-15 09:07:32 -0300

    refactor: buscando meu pet agora atraves do meu petUtils, arrumando validacao de entrada e correcao de criterios de busca

--- src/sabrina/desafio/cadastro/services/BuscarPet.java
+++ src/sabrina/desafio/cadastro/services/BuscarPet.java
@@ -3,53 +3,66 @@ package sabrina.desafio.cadastro.services;
 import sabrina.desafio.cadastro.entities.Pet;
 import sabrina.desafio.cadastro.enums.TipoPet;
 import sabrina.desafio.cadastro.services.interfaces.BuscarPetInterface;
+import sabrina.desafio.cadastro.utils.PetUtils;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
-public class BuscarPet implements BuscarPetInterface {
-    Scanner scanner = new Scanner(System.in);
+public class BuscarPet {
     BuscarPorDoisCriterios buscandoDoisCriterios = new BuscarPorDoisCriterios();
 
-    @Override
-    public List<Pet> buscandoPet(List<Pet> list, Pet pet) {
-        List<Pet> resultBusca = new ArrayList<>();
-        List<Pet> resultBuscaComCriterios = new ArrayList<>();
-        resultBusca =  buscandoCriterioObrigatorio(resultBusca, list);
-
-        System.out.println("Agora digite quantos criterios de busca você deseja [1 ou 2]: ");
-        int criterios = scanner.nextInt();
-
-        do{
-            if (criterios == 1){
-                resultBuscaComCriterios = BuscarPorUmCriterio.buscandoPorUmCriterio(scanner, resultBusca);
-            }else if (criterios == 2){
-                resultBuscaComCriterios = buscandoDoisCriterios.buscandoDoisCriterios(scanner, resultBusca);
-            }
-        }while (criterios != 1 || criterios != 2);
-
-        return resultBuscaComCriterios;
-    }
-    public List<Pet> buscandoCriterioObrigatorio(List<Pet> resultBusca, List<Pet> list){
-        System.out.println("Qual o tipo do Pet?(cachorro ou gato): ");
-        String resp = scanner.nextLine();
-        String respBusca = resp.trim().toUpperCase();
-
-        if (respBusca.equalsIgnoreCase("CACHORRO")) {
-            resultBusca = list.stream().filter(x -> x.getTipoPet().equals(TipoPet.CACHORRO)).collect(Collectors.toList());
-        } else if (respBusca.equalsIgnoreCase("GATO")) {
-            resultBusca = list.stream().filter(x -> x.getTipoPet().equals(TipoPet.GATO)).collect(Collectors.toList());
-        } else {
-            System.out.println("O tipo do pet não existe. Por favor escolha entre Gato ou Cachorro");
-        }
+    public List<Pet> buscandoPet(Scanner scanner) {
+        List<Pet> resultBusca = PetUtils.petListArquivos;
+        resultBusca =  buscandoCriterioObrigatorio(resultBusca, scanner);
         if (!resultBusca.isEmpty()) {
             System.out.println("Pet(s) encontrado(s)");
             for (int i = 0; i < resultBusca.size(); i++) {
-                System.out.println((i + 1) + ". " + resultBusca.get(i));
+                System.out.println(resultBusca.get(i));
             }
         }
+        System.out.println("Agora digite quantos criterios de busca você deseja [1 ou 2]: ");
+        int criterios = scanner.nextInt();
+
+        do {
+            if(criterios == 1 || criterios == 2) {
+                if (criterios == 1) {
+                    resultBusca = BuscarPorUmCriterio.buscandoPorUmCriterio(scanner, resultBusca);
+                } else {
+                    resultBusca= buscandoDoisCriterios.buscandoDoisCriterios(scanner, resultBusca);
+                }
+                break;
+            }else {
+                System.out.println("O resultado da busca foi inválido, escolha entre 1 e 2 e tente novamente");
+                criterios = scanner.nextInt();
+            }
+        }while (criterios == 1 || criterios == 2);
+
+        if (!resultBusca.isEmpty()) {
+            System.out.println("Pet(s) encontrado(s)");
+            for (Pet resultBuscas : resultBusca) {
+                System.out.println(resultBuscas);
+            }
+        }
+        return resultBusca;
+    }
+    public List<Pet> buscandoCriterioObrigatorio(List<Pet> resultBusca, Scanner scanner) throws InputMismatchException {
+
+        System.out.println("Qual o tipo do Pet? (cachorro ou gato): ");
+        String resp = scanner.nextLine().trim().toUpperCase();
+
+        //TODO lidar com com entrada invalida
+        do {
+            if (resp.equalsIgnoreCase("CACHORRO")) {
+                return resultBusca.stream().filter(x -> x.getTipoPet().equals(TipoPet.CACHORRO)).collect(Collectors.toList());
+            } else if (resp.equalsIgnoreCase("GATO")) {
+                return resultBusca.stream().filter(x -> x.getTipoPet().equals(TipoPet.GATO)).collect(Collectors.toList());
+            }
+            System.out.println("Tipo de animal inválido, digite somente Cachorro ou Gato");
+            resp = scanner.nextLine().trim().toUpperCase();
+        }while (resp.equalsIgnoreCase("CACHORRO") || resp.equalsIgnoreCase("GATO"));
         return resultBusca;
     }
 }
